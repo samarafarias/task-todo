112d486119acfa2df3a48d0c4e97d25e47323ddb
Sam <samara000@gmail.com>
2024-08-13 14:26:39 -0300

    add status wait

--- js/script.js
+++ js/script.js
@@ -25,10 +25,27 @@ const checkDB = () => {
 const newTask = (task, status,index) => {
     const item = document.createElement('div');
     item.classList.add('todo_item');
-    item.innerHTML = `<input type="checkbox" ${status} class="checkbox-task" data-index=${index}>
+    item.innerHTML = `<i class="fa-solid fa-circle-check" data-index=${index}></i>
         <div>${task}</div>
-        <i class="fa-solid fa-trash" data-index=${index}></i>`;
+        <div style="width: 10px;"></div>
+        <i class="fa-solid fa-clock" title="on wait" data-index=${index}></i>
+        <i class="fa-solid fa-trash" title="delete" data-index=${index}></i>`;
     document.getElementById('todoList').appendChild(item);
+
+    if (status === 'checked') {
+        const clockIcon = item.querySelector('.fa-circle-check');
+        if (clockIcon) {
+            clockIcon.classList.add('check-task');
+        }
+    }
+   
+    if (status === 'wait') {
+        const clockIcon = item.querySelector('.fa-clock');
+        if (clockIcon) {
+            clockIcon.classList.add('highlight-icon');
+        }
+    }
+
 }
 
 const deleteTask = () =>{
@@ -37,11 +54,12 @@ const deleteTask = () =>{
         todoList.removeChild(todoList.lastChild);
     }
 }
+
 const refresh = () => {
     deleteTask();
     const db = getDB();
     db.forEach ((item, index) => newTask (item.task, item.status,index));
-    checkDB(); 
+    checkDB();
 }
 
 const addTask = (event) => {
@@ -65,7 +83,19 @@ const deleteTaskDB = (index) => {
     refresh();
 }
 
-const atualizarItem = (index) => {
+const waitTask = (index) => {
+    const db = getDB();
+    db[index].status = db [index].status === ''? 'wait' : '';
+    if (db[index].status === 'wait'){
+        const [task] = db.splice(index, 1);
+        db.push(task);
+        refresh();
+    }
+    setDB(db);
+    refresh();
+}
+
+const refreshItem = (index) => {
     const db = getDB();
     db[index].status = db [index].status === ''? 'checked' : '';
     if (db[index].status === 'checked'){
@@ -75,27 +105,36 @@ const atualizarItem = (index) => {
     setDB(db);
     refresh();
 }
+
 const clickItem = (event) => {
     const element = event.target;
     if (element.classList.contains('fa-trash')) {
         const index = element.dataset.index;
-        let confirmQuestion = confirm("Are you sure you want to delete this task?");
+        const confirmQuestion = confirm("Are you sure you want to delete this task?");
         if (confirmQuestion){
             deleteTaskDB(index);
         }else{
             refresh();
             return;
         }
-    }else if (element.classList.contains('checkbox-task')){
+    }else if (element.classList.contains('fa-circle-check')){
         const index = element.dataset.index;
-        atualizarItem (index);
+        refreshItem (index);
         event.stopPropagation(); 
+    }else if (element.classList.contains('fa-clock')){
+        const index = element.dataset.index;
+        const confirmQuestion = confirm("Are you sure you want to put this task on hold?");
+        if (confirmQuestion){
+            waitTask(index);
+        }else{
+            refresh();
+            return;
+        } 
     }
 }
 
 const deleteAllTasks = () => { 
     let confirmQuestion = confirm("Are you sure you want to delete all tasks?");
-
     if (confirmQuestion){
         setDB([]);
         refresh();
@@ -104,7 +143,6 @@ const deleteAllTasks = () => {
         return;
     }
 }
-
 
 document.getElementById('newItem').addEventListener('keypress',addTask);
 document.getElementById('todoList').addEventListener('click',clickItem);
